4db84825af3a5844a8f9be3fed53d5fdf9f9e328
arasto <Arasto.Sahbaei@gmail.com>
2021-01-26 08:37:37 +0100

    m

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import './shared/css/Global.css'
 import { Routes } from './routes/Routes'
+import { UserProvider } from './shared/provider/UserProvider'
 import { DesktopNavigation } from './components/navigation/desktopnavigation/DesktopNavigation'
 
 function App() {
 	return (
-		<Routes>
-			<DesktopNavigation />
-		</Routes>
+		<UserProvider>
+			<Routes>
+				<DesktopNavigation />
+			</Routes>
+		</UserProvider>
 	)
 }
 
