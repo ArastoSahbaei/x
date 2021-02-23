f2a0a7b151b6c9071671aa57f04374195ad112a3
arasto <Arasto.Sahbaei@gmail.com>
2021-02-23 16:47:59 +0100

    m

--- server/Server.js
+++ server/Server.js
@@ -1,14 +1,31 @@
 import express from 'express'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import mongoose from 'mongoose'
 
 const application = express()
+application.use(helmet())
+application.use(morgan('common'))
+
+const checkIfAdmin = (request, response, next) => {
+	console.log(request.query)
+	next()
+}
 
 application.get('/recipe', (request, response) => {
 	response.send('Ditt API anrop gick igenom!')
 })
 
-application.get('/throwdice', (request, response) => {
+application.get('/throwdice', checkIfAdmin, (request, response) => {
 	response.send(Math.random().toString())
 })
+
+mongoose.connect('mongodb://localhost/namndb', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+	.then(() => console.log('SUCCESSFULLY CONNECTED TO THE DATABASE'))
+	.catch((error) => {
+		console.log('ERROR WHILE TRYING TO CONNECT TO THE DATABASE: ' + error)
+		process.exit()
+	})
 
 application.listen(3001, () => {
 	console.log('Servern är igång på port ' + 3001)
