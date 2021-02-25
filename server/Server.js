7bf42c291f1fae6de1052eb1d4bb55338361d6c6
arasto <Arasto.Sahbaei@gmail.com>
2021-02-25 16:36:55 +0100

    configurations done

--- server/Server.js
+++ server/Server.js
@@ -1,32 +1,15 @@
 import express from 'express'
 import helmet from 'helmet'
 import morgan from 'morgan'
-import mongoose from 'mongoose'
+import Middlewares from './src/middlewares/Middlewares.js'
+import Configuration from './configurations/Configurations.js'
 
 const application = express()
 application.use(helmet())
 application.use(morgan('common'))
 
-const checkIfAdmin = (request, response, next) => {
-	console.log(request.query)
-	next()
-}
+application.use(Middlewares.notFound)
+application.use(Middlewares.errorHandler)
 
-application.get('/recipe', (request, response) => {
-	response.send('Ditt API anrop gick igenom!')
-})
-
-application.get('/throwdice', checkIfAdmin, (request, response) => {
-	response.send(Math.random().toString())
-})
-
-mongoose.connect('mongodb://localhost/namndb', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-	.then(() => console.log('SUCCESSFULLY CONNECTED TO THE DATABASE'))
-	.catch((error) => {
-		console.log('ERROR WHILE TRYING TO CONNECT TO THE DATABASE: ' + error)
-		process.exit()
-	})
-
-application.listen(3001, () => {
-	console.log('Servern är igång på port ' + 3001)
-})
+Configuration.connectToDatabase()
+Configuration.connectToPort(application)
