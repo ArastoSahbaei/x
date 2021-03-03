06a5cddd63f62c3b9e72701d134f998b2ada3d2b
arasto <Arasto.Sahbaei@gmail.com>
2021-03-03 08:48:35 +0100

    m

--- server/src/controllers/User.controller.js
+++ server/src/controllers/User.controller.js
@@ -1,4 +1,5 @@
 import UserModel from '../models/User.model.js'
+import StatusCode from '../../configurations/StatusCode.js'
 
 const createUser = async (request, response) => {
 	const user = new UserModel({
@@ -9,9 +10,9 @@ const createUser = async (request, response) => {
 
 	try {
 		const databaseResponse = await user.save()
-		response.status(201).send(databaseResponse)
+		response.status(StatusCode.CREATED).send(databaseResponse)
 	} catch (error) {
-		response.status(500).send({
+		response.status(StatusCode.INTERNAL_SERVER_ERROR).send({
 			message: 'Error while trying to create user',
 			stack: error
 		})
@@ -21,13 +22,72 @@ const createUser = async (request, response) => {
 const getAllUsers = async (request, response) => {
 	try {
 		const databaseResponse = await UserModel.find()
-		response.status(200).send(databaseResponse)
+		response.status(StatusCode.OK).send(databaseResponse)
 	} catch (error) {
-		response.status(500).send({ message: error.message })
+		response.status(StatusCode.INTERNAL_SERVER_ERROR).send({ message: error.message })
+	}
+}
+
+const deleteUser = async (request, response) => {
+	try {
+		const userId = request.params.userId
+		const databaseResponse = await UserModel.findByIdAndDelete(userId)
+		response.status(StatusCode.OK).send({ message: 'Sucessfully deleted user', data: databaseResponse })
+	} catch (error) {
+		response.status(StatusCode.INTERNAL_SERVER_ERROR).send({
+			message: `Error while trying to delete user with ID ${userId}`,
+			error: error.message
+		})
+	}
+}
+
+const updateUser = async (request, response) => {
+	const userId = request.params.userId
+	const data = {
+		username: request.body.username,
+		password: request.body.password
+	}
+
+	try {
+		const databaseResponse = await UserModel.findByIdAndUpdate(userId, data, { new: true })
+		response.status(StatusCode.OK).send(databaseResponse)
+	} catch (error) {
+		response.status(StatusCode.INTERNAL_SERVER_ERROR).send({
+			message: `Error while trying to delete user with ID ${userId}`,
+			error: error.message
+		})
+	}
+}
+
+const queryUsername = async (request, response) => {
+	try {
+		const databaseResponse = await UserModel.find({ username: request.query.username })
+		response.status(StatusCode.OK).send(databaseResponse)
+	} catch (error) {
+		response.status(StatusCode.INTERNAL_SERVER_ERROR).send({
+			message: `Error occured while trying to retrieve user with username: ${request.query.username}`,
+			error: error.message
+		})
+	}
+}
+
+const getUserByID = async (request, response) => {
+	try {
+		const databaseResponse = await UserModel.findById({ _id: request.params.userId })
+		response.status(StatusCode.OK).send(databaseResponse)
+	} catch (error) {
+		response.status(StatusCode.INTERNAL_SERVER_ERROR).send({
+			message: `Error occured while trying to retrieve user with the ID: ${request.params.userId}`,
+			error: error.message
+		})
 	}
 }
 
 export default {
 	createUser,
-	getAllUsers
+	getAllUsers,
+	deleteUser,
+	updateUser,
+	queryUsername,
+	getUserByID
 }
